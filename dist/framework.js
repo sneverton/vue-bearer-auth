0c13c9857289779c3f0c06e54dc735239d3317e4
Everton Andrade <everton@andrade.mat.br>
2021-10-17 13:25:35 -0400

    Revert "build: release v1.2.0"
    This reverts commit 3d935616d64f1e9a4feb582df9ecc2c50528805f.

--- dist/framework.js
+++ dist/framework.js
@@ -7,36 +7,22 @@ class Framework {
         this.$config = merge({}, preset, options.config);
         this.state = Vue.observable({
             on: false,
+            checking: true,
             user: null,
-            checking: false,
-            loggingIn: false,
-            loggingOut: false,
         });
     }
     login(data, token, user) {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.state.on)
-                throw new Error("O usuário já está on.");
-            this.state.on = false;
-            this.state.loggingIn = true;
-            try {
-                const res = yield this.$axios.post(this.$config.routes.login, data);
+            return this.$axios.post(this.$config.routes.login, data).then((res) => {
                 localStorage.setItem(this.$config.localStorageKey, token(res.data));
-                this.state.on = true;
                 if (user)
                     this.state.user = user(res.data);
                 return res;
-            }
-            finally {
-                this.state.loggingIn = false;
-            }
+            });
         });
     }
     check(user) {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.state.on)
-                throw new Error("O usuário já está on.");
-            this.state.checking = true;
             const token = localStorage.getItem(this.$config.localStorageKey);
             if (!token) {
                 this.state.checking = false;
@@ -44,37 +30,27 @@ class Framework {
                 return null;
             }
             const headers = { Authorization: `Bearer ${token}` };
-            try {
-                const res = yield this.$axios.post(this.$config.routes.check, null, {
-                    headers,
-                });
+            return this.$axios
+                .post(this.$config.routes.check, null, { headers })
+                .then((res) => {
+                this.state.checking = false;
                 this.state.on = true;
                 if (user)
                     this.state.user = user(res.data);
                 return res;
-            }
-            finally {
-                this.state.checking = false;
-            }
+            });
         });
     }
     logout() {
         return __awaiter(this, void 0, void 0, function* () {
             if (!this.state.on)
-                throw new Error("O usuário já está off.");
-            if (!this.state.on)
                 return null;
-            this.state.loggingOut = true;
-            try {
-                const res = yield this.$axios.post(this.$config.routes.logout);
+            return this.$axios.post(this.$config.routes.logout).then((res) => {
                 this.state.on = false;
                 this.state.user = null;
                 localStorage.removeItem(this.$config.localStorageKey);
                 return res;
-            }
-            finally {
-                this.state.loggingOut = false;
-            }
+            });
         });
     }
 }
